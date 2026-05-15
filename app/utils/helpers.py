8551493730bade21ddf6b37f04404f554123b718
Sushant Garudkar <garudkar.sa@gmail.com>
2026-05-16 01:43:00 +0530

    codecrafters submit [skip ci]

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,12 +1,12 @@
 import os
 
-builtins = ["echo", "type", "exit"]
+builtins = ["echo", "type", "exit", "pwd"]
 path_sep = os.pathsep
 path_dirs = os.environ["PATH"].split(sep=path_sep)
 
 def is_executable_command_in_path(user_input):
     command_name = user_input.split()[0]  # the command user has called xyz ar1 ar2 then xyz
-    
+
     for path in path_dirs:
         full_path = os.path.join(path, command_name)
         if os.path.exists(path=full_path) and os.access(path=full_path, mode=os.X_OK):
