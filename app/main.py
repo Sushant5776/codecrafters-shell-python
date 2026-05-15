61a1a30c556e2c6d7dd51de0944a4b0b31751596
Sushant Garudkar <garudkar.sa@gmail.com>
2026-05-15 18:14:51 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,28 @@
 import sys
 
+# dict of builtin commands
+commands = {
+    "exit": lambda userInput: sys.exit(0),
+    "echo": lambda userInput: print(userInput[5:]),
+    "type": lambda userInput: print(f"{args} is a shell builtin")
+    if (args := userInput[5:]) in commands
+    else print(f"{args}: not found"),
+}
+
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
+    # REPL loop
     while True:
+        # Print prompt
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
-            break
-        elif command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            builtins = ["echo", "exit", "type"]
-            if command[5:] in builtins:
-                print(f"{command[5:]} is a shell builtin")
-            else:
-                print(f"{command[5:]}: not found")
+
+        # wait for user input
+        userInput = input()
+
+        if (command := userInput.split()[0]) in commands:
+            commands[command](userInput)
         else:
-            print(f"{command}: command not found")
+            print(f"{userInput}: command not found")
 
 
 if __name__ == "__main__":
